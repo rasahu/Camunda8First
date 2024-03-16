fbe6061bbeaf268b5f52a697082423d879ecbcb2
rasahu <sahu.rakesh.28@gmail.com>
2024-03-16 21:09:46 +0530

    Updated code

---
+++ src/main/java/io/camunda/onboarding/process/controller/ProcessConstants.java
@@ -0,0 +1,8 @@
+package io.camunda.onboarding.process.controller;
+class ProcessConstants {
+
+  public static final String VAR_SCORING_REQUEST_ID = "scoringRequestId";
+  public static final String VAR_SCORING_RETRY_COUNT = "scoringRetryCount";
+  public static final String VAR_SCORING_RESULT = "scoringResult";
+
+}
